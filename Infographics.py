81233243f93da6265387684e3d4047cde8a7b3d8
Saundra Johnson <49596769+SaundraMJohnson@users.noreply.github.com>
2021-07-07 16:52:22 -0400

    Added functions
    - Refactored print statements into working functions

--- Infographics.py
+++ Infographics.py
@@ -1,31 +1,35 @@
 
-#This is the infographics of input, minus the print statements.
-from sys import argv
 
-def troll_loop1(user_name, likes, lives, computer, dog):
-	print(f"Hi {user_name}")
-	print("I'd like to ask you a few questions.")
-	print(f"Do you like me {user_name}?")
+def infographics1(age, height, weight):
+	print(f"""
+		So, let me get this straight... you're {age} 
+		years old, {height} inches tall, and {weight} 
+		pounds?
+		""" )
 
-likes = input(answer)
-
-print(f"'Where do you live {user_name}?")
-lives = input(answer)
-
-print("What kind of computer do you have?")
-computer = input(answer)
-
-print(f"What kind of dog is {dog}?")
-animal = input(answer)
-
-print(f"""
-Alright, so you said {likes} about liking me.
-You live in {lives}. Not sure where that is.
-And you have a {computer} computer. Trash.
-While having {dog}, the smelly {animal}?
-Good luck in life buddy.
-""")
+def infographics2(user_name, likes, address, employer, age):
+	print(f"""
+		Alright, so you said {likes} about liking me.
+		You live in {address}. Sounds dirty.
+		And you work at {employer}. Depressing.
+		All while being {age} years old?
+		Good luck in life buddy.
+			""")
 
 answer = '> '
 
-#This does not work 20210706, trying to refactor this into a working function.
+user_name = print("First things first, What is your name? ")
+user_name = input(answer)
+print(f"Hi {user_name}, I'd like to ask you a few questions.")
+
+age = input("How old are you? ")
+height = input("How tall are you? (in inches) ")
+weight = input("How much do you weigh? ")
+
+infographics1(age, height, weight)
+
+likes = input(f"Do you like me already {user_name}? ")
+address = input("Where do you live? ")
+employer = input("Where do you work?")
+
+infographics2(user_name, likes, address, employer, age)
